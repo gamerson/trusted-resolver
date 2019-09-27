d2be5701c8e75c56b89c764d497dc060702691b3
Gregory Amerson <gregory.amerson@liferay.com>
2019-09-27 00:16:21 -0500

    rename

---
+++ trusted.resolver.requires.license.nosigner/src/main/java/com/liferay/trusted/resolver/requires/license/RequiresLicense.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.trusted.resolver.requires.license;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Gregory Amerson
+ */
+@Component(service = RequiresLicense.class, immediate = true)
+public class RequiresLicense {
+	
+	@Activate
+	public void activated() {
+		_logger.info("I am now activated.");
+	}
+
+	
+
+	@Deactivate
+	public void deactivated() {
+		_logger.info("I am now de-activated.");
+	}
+
+	private static final Logger _logger = LoggerFactory.getLogger(RequiresLicense.class);
+
+	
+}
